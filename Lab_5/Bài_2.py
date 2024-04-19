941a2f33885abe9a03d890be21bb76beaa5d9279
LeDucAnh17A2DHKL <ldanh.dhkl17a2hn@sv.uneti.edu.vn>
2024-04-19 14:01:12 +0700

    Lab 5

--- Lab_5/Bài_2.py
+++ Lab_5/Bài_2.py
@@ -1,18 +1,12 @@
-def longest_common_substring(s1, s2):
-    matrix = [[0]*(1+len(s2)) for i in range(1+len(s1))]
-    longest, x_longest = 0, 0
-    for x in range(1, 1+len(s1)):
-        for y in range(1, 1+len(s2)):
-            if s1[x-1] == s2[y-1]:
-                matrix[x][y] = matrix[x-1][y-1] + 1
-                if matrix[x][y]>longest:
-                    longest = matrix[x][y]
-                    x_longest  = x
-            else:
-                matrix[x][y] = 0
-    return s1[x_longest-longest: x_longest]
+def tim_ky_tu_chung_ngan_nhat(chuoi1, chuoi2):
+    # Tìm các ký tự chung và lưu vào một chuỗi mới
+    ky_tu_chung = ''
+    for ky_tu in chuoi1:
+        if ky_tu in chuoi2 and ky_tu not in ky_tu_chung:
+            ky_tu_chung += ky_tu
+    return ky_tu_chung
 
-# Test the function with sample strings
-s1 = "Hello, how are you?"
-s2 = "Hello, how have you been?"
-print(longest_common_substring(s1, s2))
+# Sử dụng ví dụ chuỗi để kiểm tra
+chuoi1 = "subsequence"
+chuoi2 = "subsequent"
+tim_ky_tu_chung_ngan_nhat(chuoi1, chuoi2)
